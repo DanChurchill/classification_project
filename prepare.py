830618c53102e81706deaf79cd2c5cd402340fdc
Dan Churchill <dan.churchill.satx@gmail.com>
2022-08-15 12:25:47 -0500

    updates to prepare.py

--- prepare.py
+++ prepare.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 def prep_telco(df):
     """
     Performs preparations on the telco DataFrame in order to make it usable for data exploration and modeling
@@ -9,32 +11,51 @@ def prep_telco(df):
     prepared DataFrame
     """
     # delete index columns
-    df = df.drop(columns=['internet_service_type_id', 'payment_type_id', 'contract_type_id', 'customer_id'])
+    df = df.drop(columns=['internet_service_type_id', 'payment_type_id', 'contract_type_id'])
     
     # convert total charges from string to floats
     df.total_charges = df.total_charges.str.replace(' ', '0')
     df.total_charges = df.total_charges.astype(float)
     
-    # create dummies for binary columns and concat to df
-    dummies = pd.get_dummies(df[['churn', 'gender', 'partner', 'dependents', 'phone_service',
-                                 'paperless_billing']], drop_first=True)
+    # create dummies for columns with binary values (and drop first)
+    dummies = pd.get_dummies(df[['gender', 'partner', 'dependents', 'phone_service', 
+                             'paperless_billing', 'churn']], drop_first=True)
+
+    # rename the dummy columns as necessary and add to df
+    dummies.rename(columns = {'churn_Yes':'churn',
+                            'paperless_billing_Yes' : 'paperless_billing',
+                            'phone_service_Yes': 'phone_service',
+                            'dependents_Yes': 'dependents', 
+                            'gender_Male': 'male',
+                            'partner_Yes' : 'partner'}, inplace = True)
+    
+    # concat to df
     df = pd.concat([df, dummies], axis=1)
+
+    # for columns with 3/4 values, encode and do not drop first column
+    dummies2 = pd.get_dummies(df[['multiple_lines', 'online_security', 'online_backup', 'device_protection',
+                                'tech_support', 'streaming_tv', 'streaming_movies', 'contract_type', 
+                                'payment_type', 'internet_service_type']], drop_first=False)
+
+    # concat to df
+    df = pd.concat([df, dummies2], axis=1)
+
+    # remove extra columns
+    df = df.drop(columns=['gender', 'partner', 'dependents', 'phone_service', 'multiple_lines',
+                'online_security', 'online_backup', 'device_protection', 'tech_support', 
+                'streaming_tv', 'streaming_movies', 'paperless_billing', 'churn',
+                'contract_type', 'payment_type', 'internet_service_type'])
+
+
+    # clean up column names.  Lower case, replace spaces with underscore, and shorten add-on names
+    df.columns = map(str.lower, df.columns)
+    df.columns = df.columns.str.replace(' ', '_')
+    df.columns = df.columns.str.replace('_service', '')
+    df.columns = df.columns.str.replace("automatic", 'auto').str.replace('(', '').str.replace(')', '')
+
     
-    dummies = pd.get_dummies(df[['multiple_lines', 'online_security', 'online_backup','device_protection',
-                                 'tech_support', 'streaming_tv', 'streaming_movies', 'contract_type', 'payment_type',
-                                 'internet_service_type']], drop_first=False)
-    df = pd.concat([df, dummies], axis=1)
-    
-    # delete original columns where dummies were created
-    df = df.drop(columns=['churn', 'gender', 'partner', 'dependents', 'phone_service',
-                                 'paperless_billing', 'multiple_lines', 'online_security', 'online_backup',
-                                 'device_protection', 'tech_support', 'streaming_tv', 'streaming_movies', 
-                                 'contract_type', 'payment_type','internet_service_type'])
-    
-    # clean up column names
-    df.rename(columns={'churn_Yes' : 'churn', 'gender_Male': 'male', 'partner_Yes' : 'partner',
-                       'dependents_Yes':'dependents', 'phone_service_Yes' : 'phone_service', 
-                       'paperless_billing_yes':'paperless_billing'}, inplace=True)
+    # create column with # of add_ons
+    df['addon_count'] = df['online_security_yes'] + df['online_backup_yes'] + df['device_protection_yes'] + df['tech_support_yes'] + df['streaming_tv_yes'] + df['streaming_movies_yes']
     
     return df
     
